b287c170e3fb4da96a4c5484d6e26c8109154069
Patrick TrollipI <ptrollip@gmail.com>
2020-02-03 16:05:48 +0000

    fixed bugs and migrated products app

--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, defualt='')
+    name = models.CharField(max_length=250, default='')
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
 
