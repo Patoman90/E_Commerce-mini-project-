468731a5616646a6f75ed07802207c036311a794
Patrick TrollipI <ptrollip@gmail.com>
2020-02-04 17:18:35 +0000

    updated cart urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from .views import view_cart, add_to_cart, adjust_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
-    url(r'^add', add_to_cart, name='add_to_cart'),
-    url(r'^adjust', adjust_cart, name='adjust_cart'),
+    url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart'),
 ]
