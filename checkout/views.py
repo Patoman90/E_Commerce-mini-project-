ab299b45bd0d4d9eb5452e17b5a0378624f45118
Patrick TrollipI <ptrollip@gmail.com>
2020-02-07 12:41:24 +0000

    added the stripe.js file to the static folder

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,7 @@ stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
 def checkout(request):
-    if request.checkout == "POST":
+    if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
 
